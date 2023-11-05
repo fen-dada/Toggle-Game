6212026d63869e41904fa387ea07769a3cfe9254
fen-dada <1823215739@qq.com>
2023-11-05 19:51:40 +0800

    Initial Commit

---
+++ Toggle Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Toggle Game
+//
+//  Created by Fendada on 2023/11/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Toggle Game/Toggle_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Toggle_GameApp.swift
+//  Toggle Game
+//
+//  Created by Fendada on 2023/11/5.
+//
+
+import SwiftUI
+
+@main
+struct Toggle_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
